5286af518428a921290410b4e71d5eaed9d1fcc2
Carlin Choi <carlinchoi@gmail.com>
2023-03-24 18:50:38 -0700

    Initial Commit

---
+++ src/main/java/com/visualfinance/visualfinance/VisualFinanceApplication.java
@@ -0,0 +1,13 @@
+package com.visualfinance.visualfinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisualFinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisualFinanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/visualfinance/visualfinance/VisualFinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.visualfinance.visualfinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisualFinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
