b62950eff8012eee64752cfc9e29bec781c6dd74
Carlin Choi <carlinchoi@gmail.com>
2023-06-19 16:47:20 -0700

    red files

--- frontend/my-app/src/store/index.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-
-export function configureAppStore() {
-    return configureStore({
-      // Your store configuration options go here
-    });
-  }
-  
