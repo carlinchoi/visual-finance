ffbe18ea251a499f251359d04a96adfcf5b8f199
Carlin Choi <carlinchoi@gmail.com>
2023-06-29 18:29:53 -0700

    Logo now sends back to home and resets the searchTicker in store

--- frontend/src/components/Logo/Logo.js
+++ frontend/src/components/Logo/Logo.js
@@ -1,12 +1,40 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { resetSearchTicker } from '../../store/actions/financialStatementActions';
 import { Link } from 'react-router-dom';
 import { useTheme } from '@mui/material/styles';
 
 const Logo = () => {
   const theme = useTheme();
 
+  const dispatch = useDispatch();
+
+  const ResetHome = () => {
+    dispatch(resetSearchTicker());
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      dispatch(resetSearchTicker());
+    }
+  };
+
   return (
     <Link to="/" style={{ textDecoration: 'none' }}>
-      <div style={{ color: theme.palette.primary.dark, fontSize: '24px', fontWeight: 'bold', display: 'flex', alignItems: 'center' }}>
+      <div
+        role="button"
+        tabIndex={0}
+        onClick={ResetHome}
+        onKeyDown={handleKeyDown}
+        style={{
+          cursor: 'pointer',
+          color: theme.palette.primary.dark,
+          fontSize: '24px',
+          fontWeight: 'bold',
+          display: 'flex',
+          alignItems: 'center'
+        }}
+      >
         <img src="/assets/images/icons/ProjectLogo.webp" alt="Logo" style={{ maxHeight: '40px' }} />
         Visual Finance
       </div>
