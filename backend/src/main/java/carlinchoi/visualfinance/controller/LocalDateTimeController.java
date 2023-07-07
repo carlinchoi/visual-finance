71c67dacae162ca60f946b61c0a16411664a7176
Carlin Choi <carlinchoi@gmail.com>
2023-07-07 01:00:58 -0700

    Added localDatetime to backend

---
+++ backend/src/main/java/carlinchoi/visualfinance/controller/LocalDateTimeController.java
@@ -0,0 +1,18 @@
+package carlinchoi.visualfinance.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RestController
+@RequestMapping("/")
+public class LocalDateTimeController {
+
+    @GetMapping("/time")
+    public LocalDateTime getCurrentTime() {
+        LocalDateTime currentTime = LocalDateTime.now();
+        return currentTime;
+    }
+}
