043e5f79fff4e7c169597d155503eff9281e46a1
Carlin Choi <carlinchoi@gmail.com>
2023-07-07 18:08:34 -0700

    Fixed reset of SearchTickerInput

--- frontend/src/components/Logo/Logo.js
+++ frontend/src/components/Logo/Logo.js
@@ -1,31 +1,15 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
-import { resetSearchTicker } from '../../store/actions/financialStatementActions';
 import { Link } from 'react-router-dom';
 import { useTheme } from '@mui/material/styles';
 
 const Logo = () => {
   const theme = useTheme();
 
-  const dispatch = useDispatch();
-
-  const ResetHome = () => {
-    dispatch(resetSearchTicker());
-  };
-
-  const handleKeyDown = (event) => {
-    if (event.key === 'Enter') {
-      dispatch(resetSearchTicker());
-    }
-  };
-
   return (
     <Link to="/" style={{ textDecoration: 'none' }}>
       <div
         role="button"
         tabIndex={0}
-        onClick={ResetHome}
-        onKeyDown={handleKeyDown}
         style={{
           cursor: 'pointer',
           color: theme.palette.primary.dark,
