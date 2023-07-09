8d72f8ad7665d3cda87212ec5868302904e3e5b6
Carlin Choi <carlinchoi@gmail.com>
2023-07-08 19:28:12 -0700

    Changed logic on Hometext on the homepage

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,17 +1,14 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 import { Grid } from '@mui/material';
 import HomeText from 'components/cards/HomeText';
 import SearchBar from 'components/SearchBar';
 import MainCard from 'components/MainCard';
 
 const HomePage = () => {
-  const searchTickerInput = useSelector((state) => state.financialStatement.searchTicker);
-
   return (
     <Grid container justifyContent="center" alignItems="center" style={{ marginTop: '80px' }}>
       <Grid item xs={12}>
-        {searchTickerInput ? null : <HomeText />}
+        <HomeText />
       </Grid>
       <Grid item xs={12} md={5} lg={4}>
         <MainCard sx={{ mt: 2 }}>
