a302198153cc800bc99919267b6dd00211adb675
Carlin Choi <carlinchoi@gmail.com>
2023-07-10 13:15:33 -0700

    Added indicesData to actions, reducer

--- frontend/src/store/reducers/index.js
+++ frontend/src/store/reducers/index.js
@@ -6,13 +6,15 @@ import financialStatementReducer from './financialStatementReducer';
 import stockDataPriceReducer from './stockDataPriceReducer';
 import stockDividendsReducer from './stockDividendsReducer';
 import twelveDataReducer from './twelveDataReducer';
+import indicesDataReducer from './IndicesDataReducer';
 
 const reducers = combineReducers({
   menu,
   financialStatement: financialStatementReducer,
   stockDataPrice: stockDataPriceReducer,
   stockDividends: stockDividendsReducer,
-  twelveData: twelveDataReducer
+  twelveData: twelveDataReducer,
+  indicesData: indicesDataReducer
 });
 
 export default reducers;
