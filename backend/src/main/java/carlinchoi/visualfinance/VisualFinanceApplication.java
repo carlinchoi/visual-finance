7bc000d016d40c3460f7e7c20575ea2eb323c13c
Carlin Choi <carlinchoi@gmail.com>
2023-07-14 02:51:36 -0700

    removed changes to run aplication class

--- backend/src/main/java/carlinchoi/visualfinance/VisualFinanceApplication.java
+++ backend/src/main/java/carlinchoi/visualfinance/VisualFinanceApplication.java
@@ -2,6 +2,11 @@ package carlinchoi.visualfinance;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 
--- backend/src/main/java/carlinchoi/visualfinance/controller/IndicesController.java
+++ backend/src/main/java/carlinchoi/visualfinance/controller/IndicesController.java
@@ -3,12 +3,13 @@ package carlinchoi.visualfinance.controller;
 import carlinchoi.visualfinance.model.Indices;
 import carlinchoi.visualfinance.service.IndicesAPIService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@RequestMapping("/")
+@RequestMapping(path="")
 @CrossOrigin
 public class IndicesController {
     private final IndicesAPIService indicesAPIService;
@@ -18,7 +19,8 @@ public class IndicesController {
         this.indicesAPIService = indicesAPIService;
     }
 
-    @GetMapping("/indices")
+    @ResponseStatus(HttpStatus.OK)
+    @RequestMapping(path = "/indices", method = RequestMethod.GET)
     public ResponseEntity<Indices> getIndicesData(@RequestParam("symbol") String symbol) {
         Indices indicesData = indicesAPIService.getIndicesData(symbol);
         return ResponseEntity.ok(indicesData);
