af23538c738960ebb721102ac73c82760ff2c757
Carlin Choi <carlinchoi@gmail.com>
2023-07-18 20:16:55 -0700

    Addded stockdatanews to actions

--- frontend/src/store/actions/stockDataPriceActions.js
+++ frontend/src/store/actions/stockDataPriceActions.js
@@ -1,8 +1,16 @@
 export const FETCH_STOCK_DATA_PRICE = 'FETCH_STOCK_DATA_PRICE';
+export const FETCH_STOCK_DATA_NEWS = 'FETCH_STOCK_DATA_NEWS';
 
 export const fetchStockDataPrice = (data) => {
   return {
     type: FETCH_STOCK_DATA_PRICE,
+    payload: data
+  };
+};
+
+export const fetchStockDataNews = (data) => {
+  return {
+    type: FETCH_STOCK_DATA_NEWS,
     payload: data
   };
 };
