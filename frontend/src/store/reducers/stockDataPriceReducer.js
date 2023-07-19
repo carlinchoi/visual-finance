f365f39a0ebfde7f91c7c8c5089e51fa379cc837
Carlin Choi <carlinchoi@gmail.com>
2023-07-18 20:20:55 -0700

    Reducer for stockdatanews done

--- frontend/src/store/reducers/stockDataPriceReducer.js
+++ frontend/src/store/reducers/stockDataPriceReducer.js
@@ -1,10 +1,11 @@
-import { FETCH_STOCK_DATA_PRICE } from '../actions/stockDataPriceActions';
+import { FETCH_STOCK_DATA_NEWS, FETCH_STOCK_DATA_PRICE } from '../actions/stockDataPriceActions';
 
 const initialState = {
   stockDataPrice: [],
   loading: false,
   error: null,
-  searchTicker: ''
+  searchTicker: '',
+  stockDataNews: []
 };
 
 const stockDataPriceReducer = (state = initialState, action) => {
@@ -18,6 +19,13 @@ const stockDataPriceReducer = (state = initialState, action) => {
         error: null
       };
     }
+    case FETCH_STOCK_DATA_NEWS:
+      return {
+        ...state,
+        stockDataNews: action.payload,
+        loading: false,
+        error: null
+      };
     default:
       return state;
   }
