f39925e32c8d49749ee4b2a6eabb7b9c13b109bc
Carlin Choi <carlinchoi@gmail.com>
2023-07-24 23:23:15 -0700

    Added crypto to HomeExample. Adding functionality later

--- frontend/src/components/cards/HomeExample.js
+++ frontend/src/components/cards/HomeExample.js
@@ -52,6 +52,31 @@ function HomeExample() {
           </Typography>
         </Grid>
       </Grid>
+
+      <Grid
+        container
+        alignItems="center"
+        sx={{
+          px: { xs: 3, sm: 5, md: 10, lg: 20 },
+          py: 5,
+          backgroundColor: '#ebeff3',
+          flexDirection: { xs: 'column', md: 'row' }
+        }}
+      >
+        <Grid xs={12} md={6} sx={{ pb: { xs: 5, md: 0 }, pr: { xs: 0, md: 5 } }}>
+          <Typography fontSize={28} fontWeight={500} lineHeight={1.2}>
+            Discover crypto prices
+          </Typography>
+          <Typography paddingTop={1} fontSize={18}>
+            Be the first to know about price changes on your crypto assets. We list the top 100 cryptocurrencies by marketcap for your
+            convenience.
+          </Typography>
+        </Grid>
+        {/* Prices Pic */}
+        <Grid xs={12} md={6}>
+          <img src="/imgs/prices.jpeg" alt="Crypto Prices" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
+        </Grid>
+      </Grid>
     </Box>
   );
 }
