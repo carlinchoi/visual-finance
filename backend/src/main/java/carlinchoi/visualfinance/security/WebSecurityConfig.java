94b3aa1c6a1ab110de240086e0822bd819bbe527
Carlin Choi <carlinchoi@gmail.com>
2023-09-04 15:13:18 -0700

    Logout functional and working in postman

--- backend/src/main/java/carlinchoi/visualfinance/security/WebSecurityConfig.java
+++ backend/src/main/java/carlinchoi/visualfinance/security/WebSecurityConfig.java
@@ -1,16 +1,22 @@
 package carlinchoi.visualfinance.security;
-import carlinchoi.visualfinance.security.jwt.JWTConfigurer;
-import carlinchoi.visualfinance.security.jwt.TokenProvider;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import carlinchoi.visualfinance.security.jwt.JWTConfigurer;
+import carlinchoi.visualfinance.security.jwt.TokenProvider;
+import carlinchoi.visualfinance.security.JwtAuthenticationEntryPoint;
+import carlinchoi.visualfinance.security.JwtAccessDeniedHandler;
+import carlinchoi.visualfinance.security.UserModelDetailsService;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
@@ -47,31 +53,42 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     /**
-     * Configure com.techelevator.auctions.security settings
+     * Configure security settings
      * @param httpSecurity
      * @throws Exception
      */
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
-                // we don't need CSRF because our token is invulnerable
+                // Disable CSRF because our token is invulnerable
                 .csrf().disable()
 
                 .exceptionHandling()
                 .authenticationEntryPoint(jwtAuthenticationEntryPoint)
                 .accessDeniedHandler(jwtAccessDeniedHandler)
 
-                // create no session
+                // Create no session
                 .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 
+                // Configure logout
                 .and()
-                .apply(securityConfigurerAdapter());
+                .logout()
+                .logoutUrl("/logout") // Specify the logout URL
+                .logoutSuccessHandler((request, response, authentication) -> {
+                    // Clear the security context and return a successful response
+                    SecurityContextHolder.clearContext();
+                    response.setStatus(HttpStatus.OK.value());
+                })
+                .invalidateHttpSession(true); // Invalidate the session
+
+        // Apply JWT security filter
+        httpSecurity.apply(securityConfigurerAdapter());
     }
+
 
     private JWTConfigurer securityConfigurerAdapter() {
         return new JWTConfigurer(tokenProvider);
     }
 }
-
