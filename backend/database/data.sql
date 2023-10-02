9c8ed30d3b28e2d4d2dd333dc587d6017b82002a
Carlin Choi <carlinchoi@gmail.com>
2023-10-02 01:57:54 -0700

    Updated data.sql script to include default portfolio

--- backend/database/data.sql
+++ backend/database/data.sql
@@ -4,6 +4,10 @@ INSERT INTO users (email,password_hash,role,first_name,last_name) VALUES ('user@
 INSERT INTO users (email,password_hash,role,first_name,last_name) VALUES ('admin@gmail.com','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN','adminFirstName','adminLastName');
 INSERT INTO users (email,password_hash,role,first_name,last_name) VALUES('carlinchoi@gmail.com','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER','Carlin','Choi');
 
-
+INSERT INTO stock_portfolio (user_id,stock_name,dollars_invested,number_of_shares,stock_price) VALUES(1,'Microsoft',10000,8.3,183.30);
+INSERT INTO stock_portfolio (user_id,stock_name,dollars_invested,number_of_shares,stock_price) VALUES(1,'Apple',12000,6.3,302.24);
+INSERT INTO stock_portfolio (user_id,stock_name,dollars_invested,number_of_shares,stock_price) VALUES(1,'Google',20000,10,110.22);
+INSERT INTO stock_portfolio (user_id,stock_name,dollars_invested,number_of_shares,stock_price) VALUES(1,'Amazon',16000,7.2,993.38);
+INSERT INTO stock_portfolio (user_id,stock_name,dollars_invested,number_of_shares,stock_price) VALUES(1,'Facebook',11000,5.8,538.22);
 
 COMMIT TRANSACTION;
